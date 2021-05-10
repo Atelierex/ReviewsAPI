ce0ec41ac6b07c96c51d53469d663b22cf77fcbd
Will Trinh <williamltrinh@gmail.com>
2021-05-10 09:31:54 -0700

    Implemented queries for reviews model

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -29,3 +29,42 @@ const postReview = (review, callback) => {
     }
   })
 }
+
+const postCharacteristics = (product_id, characteristic, callback) => {
+  const queryStr = `INSERT INTO Characteristics (product_id, characteristic_name) VALUES ('${product_id}', '${characteristic})`;
+
+  db.query(queryStr, (err, res) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, res);
+    }
+  })
+}
+
+const postCharReviews = (review_id, char_id, char_value, callback) => {
+  const queryStr = `INSERT INTO Characteristic_Reviews (review_id, characteristic_id, characteristic_value)
+  VALUES ('${review_id}', '${char_id}', '${Object.values(char_value)[0]}')`;
+
+  db.query(queryStr, (err, res) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, res);
+    }
+  })
+}
+
+const postPhotos = (review_id, photo_url, callback) => {
+  const queryStr = `INSERT INTO Photos (review_id, photo_url) VALUES ('${review_id}', '${photo_url}')`;
+
+  db.query(queryStr, (err, res) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, res);
+    }
+  })
+}
+
+module.exports = { getReviews, postReview, postCharacteristics, postCharReviews, postPhotos };
