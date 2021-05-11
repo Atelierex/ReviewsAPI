2e398931d5ad10f297dcb9dd2c10823138a7003e
Will Trinh <williamltrinh@gmail.com>
2021-05-10 20:51:30 -0700

    Fixed issue where recommend and reported value are always 0 in database

--- server/dbs/sql/loadData.sql
+++ server/dbs/sql/loadData.sql
@@ -13,7 +13,9 @@ INTO TABLE Reviews
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(id,product_id,rating,review_date,summary,body,@var1,@var2,reviewer_name,reviewer_email,response,helpfulness)
+SET recommend = IF(@var1="true",1,0), reported = IF(@var2="true",1,0);
 
 -- -- Characteristics 3339443 -> 3331029
 LOAD DATA LOCAL INFILE '../../dataset/characteristics.csv' 
