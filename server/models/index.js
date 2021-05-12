fac23cb51b31245b90ab14280dfd90e3708f1c63
Will Trinh <williamltrinh@gmail.com>
2021-05-12 11:30:30 -0700

    Removed everything that is not related to DBMS

--- server/models/index.js
+++
@@ -1,4 +0,0 @@
-exports.reviews = require('./reviews.js');
-exports.reviewsMeta = require('./reviewsMeta.js');
-exports.helpful = require('./helpful.js');
-exports.report = require('./report.js');
--- tests/putReport.js
+++
@@ -1,8 +0,0 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
-
-export default function () {
-  let res = http.get('http://localhost:3000/reviews/348390/report');
-  check(res, { 'status was 200': (r) => r.status == 200 });
-  sleep(1);
-}
