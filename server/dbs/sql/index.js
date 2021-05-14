e2878f73805bdea5fffbc4ff982cf659574703fb
Will Trinh <williamltrinh@gmail.com>
2021-05-13 17:55:35 -0700

    Added more k6 ramp up test snapshots

--- server/dbs/sql/index.js
+++ server/dbs/sql/index.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-  host: 'ec2-54-193-50-253.us-west-1.compute.amazonaws.com',
-  port: 3306,
-  user: 'user1',
-  password: 'password',
-  database: 'sdc'
-});
-
-
 // const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
+//   host: 'ec2-54-193-50-253.us-west-1.compute.amazonaws.com',
+//   port: 3306,
+//   user: 'user1',
+//   password: 'password',
 //   database: 'sdc'
 // });
+
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'sdc'
+});
 db.connect((err) => {
   if (err) throw err;
   console.log('DB connected!');
--- tests/script.js
+++ tests/script.js
@@ -5,20 +5,20 @@ import { sleep } from 'k6';
 export let options = {
   thresholds: {
     http_req_failed: ['rate<0.01'], // During the whole test execution, the error rate must be lower than 1%.
-    http_req_duration: ['p(99)<2000'], // 99% of requests must complete below 2s
+    // http_req_duration: ['p(99)<2000'], // 99% of requests must complete below 2s
   },
   stages: [
-    { duration: '10s', target: 200 }, // simulate ramp-up of traffic from 1 to 100 users over 15 seconds
-    { duration: '10s', target: 700 }, // stay at 500 users for 30 seconds
-    { duration: '15s', target: 1500 }, // ramp-up to 1500 users over 30 seconds (peak hour starts)
-    { duration: '2m20s', target: 3000 }, // stay at 100 users for short amount of time (peak hour)
-    { duration: '15s', target: 1500 }, // ramp-down to 1500 users over 30 seconds (peak hour ends)
-    { duration: '10s', target: 1500 }, // continue at 500 for additional 30 seconds
-    { duration: '10s', target: 200 }, // ramp-down to 0 users
+    { duration: '10s', target: 200 }, // simulate ramp-up of traffic from 1 to 200 users over 10 seconds
+    { duration: '15s', target: 700 }, // stay at 500 users for 15 seconds
+    { duration: '30s', target: 1500 }, // ramp-up to 1500 users over 30 seconds (peak hour starts)
+    { duration: '2m', target: 3000 }, // stay at 3000 users for short amount of time (peak hour)
+    { duration: '30s', target: 1500 }, // ramp-down to 1500 users over 30 seconds (peak hour ends)
+    { duration: '15s', target: 700 }, // continue at 500 for additional 15 seconds
+    { duration: '10s', target: 200 }, // ramp-down to 200 users
   ]
 };
 export default function () {
-  http.get('http://localhost:3000/reviews?product_id=987123');
+  http.get('http://localhost:3000/reviews?product_id=987120');
   // http.get('http://localhost:3000/reviews/meta?product_id=987456');
   sleep(1);
 }
