f66e922cdc3da5ef3a3776896943ac5f8f3e44cd
Will Trinh <williamltrinh@gmail.com>
2021-05-14 13:43:19 -0700

    Upload new loader.io token

--- server/controllers/loaderio.js
+++ server/controllers/loaderio.js
@@ -1,5 +1,6 @@
 const token = ((req, res) => {
-  res.sendFile(__dirname + '/loaderio-5d886566a4788d7b18a783b471e05e20.txt');
+  // res.sendFile(__dirname + '/loaderio-5d886566a4788d7b18a783b471e05e20.txt');
+  res.sendFile(__dirname + '/loaderio-9e2243158a0580e7ebac1c56d822ad11.txt');
 });
 
 module.exports = { token };
--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { reviews, reviewsMeta, helpful, report, loaderio } = require('./controllers/index.js');
 const router = express.Router();
 
-router.get('/loaderio-5d886566a4788d7b18a783b471e05e20', loaderio.token);
+// router.get('/loaderio-5d886566a4788d7b18a783b471e05e20', loaderio.token);
+router.get('/loaderio-9e2243158a0580e7ebac1c56d822ad11', loaderio.token);
 router.get('/reviews', reviews.getReviews);
 router.get('/reviews/meta', reviewsMeta.getMetadata);
 router.post('/reviews', reviews.postReview);
